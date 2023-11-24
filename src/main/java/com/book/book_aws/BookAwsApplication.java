fd333607a0c4d2fc91787ae4c413e8d63159adb3
leeyongwoo <tiger1650@naver.com>
2023-11-24 21:01:09 +0900

    .gitignore 등록

---
+++ src/main/java/com/book/book_aws/BookAwsApplication.java
@@ -0,0 +1,13 @@
+package com.book.book_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/book_aws/BookAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.book_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
